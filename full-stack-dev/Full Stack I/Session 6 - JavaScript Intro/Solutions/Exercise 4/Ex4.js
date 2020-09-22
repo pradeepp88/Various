5362c3e4a18bf93df04c4d94a5405bfc00e04260
unknown <mike.denton.work@gmail.com>
2020-09-22 13:54:50 -0400

    added solution lab 6

---
+++ full-stack-dev/Full Stack I/Session 6 - JavaScript Intro/Solutions/Exercise 4/Ex4.js
@@ -0,0 +1,7 @@
+var a = 5;
+var b = true;
+var c = "Test string";
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ full-stack-dev/Full Stack I/Session 6 - JavaScript Intro/Solutions/Exercise 5/Ex5.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 20;
+var c = 5;
+
+console.log(a + b);
+console.log(a * b);
+console.log(b - c);
